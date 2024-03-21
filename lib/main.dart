1350b3820d3ccce934825039710fdfe28b5bc1ea
Yibing Chen <444980@student.saxion.nl>
2024-03-21 15:29:50 +0100

    main screen to check whether there is token. If  yes to the list. if no redirect to login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
-//import 'dart:js';
-
 import 'package:aad_hybrid/screens/add_item.dart';
 import 'package:aad_hybrid/screens/home.dart';
 import 'package:aad_hybrid/screens/login.dart';
-import 'package:aad_hybrid/screens/my_items.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String? token = prefs.getString('token');
 
   runApp(MaterialApp(
-
+    home: token != null ? Home() : Login(),
     routes: {
-      '/':(context)=>Login(),
-      '/addItem':(context)=>AddItem(),
+      '/addItem': (context) => AddItem(),
     },
   ));
 }
-
