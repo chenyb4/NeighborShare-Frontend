668ac0022abaaef99d5dd86ae0486fd5a0ebb597
Yibing Chen <444980@student.saxion.nl>
2024-03-21 20:05:07 +0100

    register

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,7 +1,68 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:aad_hybrid/configs/colors.dart';
 
+import '../configs/backend_address.dart';
+
 class Register extends StatelessWidget {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  Future<void> registerUser(BuildContext context) async {
+
+    try {
+      final response = await http.post(
+        Uri.parse(baseUrl + '/users'),
+        body: jsonEncode({
+          'name': nameController.text.trim(),
+          'email': emailController.text.trim(),
+          'password': passwordController.text.trim(),
+        }),
+        headers: {'Content-Type': 'application/json'},
+      );
+
+      if (response.statusCode == 201) {
+        // Registration successful
+        // You can navigate to another screen or show a success message
+        // Registration successful
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Registration successful!'),
+            duration: Duration(seconds: 2), // Adjust duration as needed
+          ),
+        );
+        // Navigate to login screen after a short delay
+        Future.delayed(Duration(seconds: 2), () {
+          Navigator.pushReplacementNamed(context, '/login');
+        });
+      } else {
+        // Registration failed
+        // You can show an error message
+        // Registration failed
+        final responseBody = jsonDecode(response.body);
+        final errorMessage = responseBody['message'];
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Registration failed: $errorMessage'),
+            duration: Duration(seconds: 2), // Adjust duration as needed
+          ),
+        );
+      }
+    } catch (e) {
+      // Handle any errors that occur during the process
+      // Handle any errors that occur during the process
+      print('Error registering user: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('An error occurred. Please try again later.'),
+          duration: Duration(seconds: 2), // Adjust duration as needed
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,18 +77,21 @@ class Register extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextField(
+              controller: nameController,
               decoration: InputDecoration(
                 labelText: 'Name',
               ),
             ),
             SizedBox(height: 20.0),
             TextField(
+              controller: emailController,
               decoration: InputDecoration(
                 labelText: 'Email',
               ),
             ),
             SizedBox(height: 20.0),
             TextField(
+              controller: passwordController,
               obscureText: true,
               decoration: InputDecoration(
                 labelText: 'Password',
@@ -36,10 +100,10 @@ class Register extends StatelessWidget {
             SizedBox(height: 50.0),
             ElevatedButton(
               onPressed: () {
-                // Implement registration functionality
+                registerUser(context); // Call the registerUser function
               },
               child: Text(
-                  'Register',
+                'Register',
                 style: TextStyle(color: Colors.white),
               ),
               style: ElevatedButton.styleFrom(
