075aa6cbcc4d07ccc4719ef5270ee96440de77de
Yibing Chen <444980@student.saxion.nl>
2024-03-21 11:15:36 +0100

    first step of login functionality. not doing login call or register call yet

--- lib/configs/backend_address.dart
+++ lib/configs/backend_address.dart
@@ -1,4 +1,11 @@
 
+//android physical device
+//const String baseUrl = 'http://192.168.1.102:3000';
 
 
+//android emulator
 const String baseUrl = 'http://10.0.2.2:3000';
+
+
+//other
+//const String baseUrl = 'http://localhost:3000';
