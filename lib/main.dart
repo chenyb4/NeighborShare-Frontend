ceccd4e35141c028cac3b61f4f023ec99f365ca8
Yibing Chen <444980@student.saxion.nl>
2024-03-21 15:18:02 +0100

    basic login and register screens without doing actual calls

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:aad_hybrid/screens/add_item.dart';
 import 'package:aad_hybrid/screens/home.dart';
+import 'package:aad_hybrid/screens/login.dart';
 import 'package:aad_hybrid/screens/my_items.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +12,7 @@ void main() {
   runApp(MaterialApp(
 
     routes: {
-      '/':(context)=>Home(),
+      '/':(context)=>Login(),
       '/addItem':(context)=>AddItem(),
     },
   ));
---
+++ lib/screens/login.dart
@@ -0,0 +1,67 @@
+import 'package:aad_hybrid/screens/register.dart';
+import 'package:flutter/material.dart';
+import 'package:aad_hybrid/configs/colors.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("NeighborShare"),
+        backgroundColor: themeColorShade1,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Login',
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+            ),
+            SizedBox(height: 50.0),
+            ElevatedButton(
+              onPressed: () {
+                // Implement login functionality
+              },
+              child: Text(
+                  'Login',
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: themeColorShade1,
+              ),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Register()),
+                );
+              },
+              child: Text('Register'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
