af631453381bccc1b6697969ab4de127964c59ae
Yibing Chen <444980@student.saxion.nl>
2024-03-23 15:49:05 +0100

    how my apartment on the banner

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,12 +2,10 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
-import '../data/data.dart';
 import '../models/Item.dart';
 import 'item_details.dart';
 import 'my_items.dart';
 import 'package:aad_hybrid/configs/colors.dart';
-import 'package:aad_hybrid/screens/add_item.dart';
 import 'package:aad_hybrid/configs/backend_address.dart';
 
 class Home extends StatefulWidget {
@@ -59,6 +57,47 @@ class _HomeState extends State<Home> {
       throw Exception('Failed to load items');
     }
   }
+
+  Future<String> _fetchApartmentName() async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String token = prefs.getString('token') ?? '';
+      Map<String, dynamic> payload = _parseJwt(token);
+      String userEmail = payload['email'] ?? '';
+
+      final userResponse = await http.get(
+        Uri.parse(baseUrl + '/users?email=$userEmail'),
+        headers: {'Authorization': 'Bearer $token'},
+      );
+
+      if (userResponse.statusCode == 200) {
+        List<dynamic> users = jsonDecode(userResponse.body);
+        if (users.isNotEmpty) {
+          dynamic apartmentId = users[0]['apartment_id']; // Change apartmentId type to dynamic
+          final apartmentResponse = await http.get(
+            Uri.parse(baseUrl + '/apartments/$apartmentId'),
+            headers: {'Authorization': 'Bearer $token'},
+          );
+
+          if (apartmentResponse.statusCode == 200) {
+            Map<String, dynamic> apartmentData = jsonDecode(apartmentResponse.body);
+            return apartmentData['name'] ?? 'Unknown Apartment';
+          } else {
+            throw Exception('Failed to load apartment');
+          }
+        } else {
+          throw Exception('User not found');
+        }
+      } else {
+        throw Exception('Failed to load user');
+      }
+    } catch (error) {
+      print('Error fetching apartment name: $error');
+      return 'Unknown Apartment';
+    }
+  }
+
+
 
   Future<void> _logout() async {
     bool confirmLogout = await showDialog(
@@ -136,90 +175,124 @@ class _HomeState extends State<Home> {
         ),
       ),
       body: Center(
-        child: RefreshIndicator(
-          onRefresh: () {
-            setState(() {
-              futureItems = fetchItems();
-            });
-            return futureItems;
-          },
-          child: FutureBuilder<List<Item>>(
-            future: futureItems,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return CircularProgressIndicator();
-              } else if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              } else if (snapshot.hasData) {
-                List<Item> myItems = snapshot.data!
-                    .where((item) => item.ownerEmail != myEmail)
-                    .toList();
-                return ListView.builder(
-                  itemCount: myItems.length,
-                  itemBuilder: (context, index) {
-                    Item item = myItems[index];
-                    return Column(
-                      children: [
-                        ListTile(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) =>
-                                    ItemDetails(item: item),
-                              ),
-                            );
-                          },
-                          title: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                item.name,
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                              Row(
-                                children: [
-                                  Icon(Icons.location_on),
-                                  Text(item.apartmentNumber),
-                                ],
-                              ),
-                            ],
-                          ),
-                          subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(item.description),
-                              Text(
-                                item.isAvailable
-                                    ? "Available"
-                                    : "Unavailable",
-                                style: TextStyle(
-                                  color: item.isAvailable
-                                      ? Colors.green
-                                      : Colors.red,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
-                          ),
-                          tileColor: themeColorShade2,
-                        ),
-                        const Divider(
-                          height: 1,
-                          thickness: 1,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              color: Colors.blue, // You can customize the color as needed
+              padding: EdgeInsets.symmetric(vertical: 8.0),
+              child: FutureBuilder<String>(
+                future: _fetchApartmentName(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else if (snapshot.hasData) {
+                    return Center(
+                      child: Text(
+                        'Apartment: ${snapshot.data}',
+                        style: TextStyle(
                           color: Colors.white,
+                          fontWeight: FontWeight.bold,
                         ),
-                      ],
+                      ),
                     );
+                  } else {
+                    return SizedBox.shrink(); // Hide the banner if no data is available
+                  }
+                },
+              ),
+            ),
+            Expanded(
+              child: RefreshIndicator(
+                onRefresh: () {
+                  setState(() {
+                    futureItems = fetchItems();
+                  });
+                  return futureItems;
+                },
+                child: FutureBuilder<List<Item>>(
+                  future: futureItems,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return CircularProgressIndicator();
+                    } else if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    } else if (snapshot.hasData) {
+                      List<Item> myItems = snapshot.data!
+                          .where((item) => item.ownerEmail != myEmail)
+                          .toList();
+                      return ListView.builder(
+                        itemCount: myItems.length,
+                        itemBuilder: (context, index) {
+                          Item item = myItems[index];
+                          return Column(
+                            children: [
+                              ListTile(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          ItemDetails(item: item),
+                                    ),
+                                  );
+                                },
+                                title: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      item.name,
+                                      style: TextStyle(fontWeight: FontWeight.bold),
+                                    ),
+                                    Row(
+                                      children: [
+                                        Icon(Icons.location_on),
+                                        Text(item.apartmentNumber),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                subtitle: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(item.description),
+                                    Text(
+                                      item.isAvailable
+                                          ? "Available"
+                                          : "Unavailable",
+                                      style: TextStyle(
+                                        color: item.isAvailable
+                                            ? Colors.green
+                                            : Colors.red,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                tileColor: themeColorShade2,
+                              ),
+                              const Divider(
+                                height: 1,
+                                thickness: 1,
+                                color: Colors.white,
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    } else {
+                      return Text('No data available');
+                    }
                   },
-                );
-              } else {
-                return Text('No data available');
-              }
-            },
-          ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.pushNamed(context, '/addItem').then((value) {
